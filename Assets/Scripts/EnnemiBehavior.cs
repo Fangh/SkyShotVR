d82c8bc609d11ee4e72f31473ff290ffdd39debe
Fangh <toto.klein@laposte.net>
2018-02-11 12:15:12 +0100

    ennemies are ok

--- Assets/Scripts/EnnemiBehavior.cs
+++ Assets/Scripts/EnnemiBehavior.cs
@@ -6,12 +6,18 @@ public class EnnemiBehavior : MonoBehaviour
 {
 	PlayerController player;
 	Rigidbody rb;
-	float distanceFromPlayer;
+	float distanceFromPlayer = 0;
+	bool playerIsLocked = false;
+	Vector3 destination = Vector3.zero;
+	bool stopMoving = false;
+	float originalDrag = 0;
+	
 
 	[Header("Balancing")]
 	public float distanceMaxFromPlayer = 30f;
 	public float distanceMinFromPlayer = 2f;
 	public float distanceToShotPlayer = 3f;
+	public float distanceToLockPlayer = 10f;
 	public float movementSpeed = 4f;
 	public float rotationSpeed = 1f;
 
@@ -19,23 +25,95 @@ public class EnnemiBehavior : MonoBehaviour
 	void Start () 
 	{
 		player = PlayerController.Instance;
-		rb = GetComponent<Rigidbody>();		
+		rb = GetComponent<Rigidbody>();	
+		originalDrag = rb.drag;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		distanceFromPlayer = Vector3.Distance(transform.position, player.transform.position);
-		if (di)
+		distanceFromPlayer = Vector3.Distance( transform.position, player.transform.position );
+		Debug.DrawLine( player.transform.position, destination, Color.red, 1f );
+
+		//if you are far from player, move freely
+		if ( distanceFromPlayer > distanceToLockPlayer && !playerIsLocked )
+		{
+			Debug.Log("[Ennemi] Moving around", this);
+			MoveAround();
+		}
+		//if you are near player, go toward them.
+		else if ( distanceFromPlayer > distanceMinFromPlayer )
+		{
+			Debug.Log("[Ennemi] Locking Player", this);
+			//lookatPlayerAnd go toward them. And don't move freely ever again
+			LockPlayer();
+		}
+		else
+		{
+			Debug.Log("[Ennemi] Stop Moving", this);
+			// Debug.Log( "distanceFromPlayer = " + distanceFromPlayer );
+			stopMoving = true;
+		}
+		// if you are at range, shoot !
+		if ( distanceFromPlayer < distanceToShotPlayer )
+		{
+			ShootAtPlayer();
+		}
+	}
+
+	//I choosed to move the ennemi with forces to make it drift. So it may not be 4m/s
+	//to make it move at 4m/s I should have use transform.Translate(forward * speed * Time.deltaTime)
+	void FixedUpdate()
+	{
+		if ( stopMoving )
+		{
+			rb.drag = 10f;
+		}
+		else
+		{
+			rb.drag = originalDrag;
+			if ( destination != Vector3.zero)
+			{
+				Debug.Log("[Ennemi] moving toward destination", this);
+				rb.AddRelativeForce( Vector3.forward * movementSpeed );
+			}
+		}
 	}
 
 	void MoveAround()
 	{
-
+		//if you do not have a destination, take one random not too far from player
+		if ( destination == Vector3.zero )
+		{
+			destination = Random.insideUnitSphere * distanceMaxFromPlayer;
+			destination += player.transform.position;
+		}
+		if (Vector3.Distance( transform.position, destination) < 1f )
+		{
+			destination = Vector3.zero;
+		}
+		SmoothLookAt(destination);
+		stopMoving = false;
 	}
 
 	void LockPlayer()
 	{
+		playerIsLocked = true;
+		destination = player.transform.position;
+		stopMoving = false;
+		SmoothLookAt( destination );
+	}
 
+	void ShootAtPlayer()
+	{
+		SmoothLookAt( player.transform.position );
+		Debug.Log("pew pew");
+	}
+
+	void SmoothLookAt(Vector3 target)
+	{
+		Vector3 relativePos = target - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+		transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
 	}
 }
